fdf14ccdd2b3db471e0ea5359f2c7dcffe7d31f4
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-10-17 20:51:59 -0300

    wrapped agent code in a class and a run function, also imported and instantiade dqn

--- agent.py
+++ agent.py
@@ -1,21 +1,35 @@
+import torch
 import flappy_bird_gymnasium
 import gymnasium
+from dqn import DQN
 
-# env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
-env = gymnasium.make("CartPole-v1", render_mode="human")
 
-obs, _ = env.reset()
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-while True:
-  # Next action
-  # (feed the observation to your agent here)
-  action = env.action_space.sample()
+class Agent:
+    
+  def run(self, is_training = True, render=False):
+    # env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
+    env = gymnasium.make("CartPole-v1", render_mode="human")
 
-  # processing
-  obs, reward, terminated, _, info = env.step(action)
+    num_actions = env.action_space.n
 
-  # checking if the player is still alive
-  if terminated:
-    break
+    num_states = env.observation_space.shape[0]
 
-env.close()
+    policy_dqn = DQN(num_states, num_actions).to(device)
+
+    obs, _ = env.reset()
+
+    while True:
+      # Next action
+      # (feed the observation to your agent here)
+      action = env.action_space.sample()
+
+      # processing
+      obs, reward, terminated, _, info = env.step(action)
+
+      # checking if the player is still alive
+      if terminated:
+        break
+
+    env.close()
