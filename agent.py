8ddd65cadf0de95338ae6888c7ecea27782a13c1
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-10-17 21:17:10 -0300

    In this commit the agent class was modified to: - save the state, reward and next state generated by a action - the loop was modified to work by episodes - the agent now loads the hyperparameters by a yaml file - now we memorize the rewards obtained in each episode

--- agent.py
+++ agent.py
@@ -1,12 +1,29 @@
+import yaml
 import torch
-import flappy_bird_gymnasium
+import itertools
 import gymnasium
 from dqn import DQN
+import flappy_bird_gymnasium
+from experience_replay import ReplayMemory
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 class Agent:
+
+  def __init__(self, hyperparameter_set) -> None:
+    
+    with open("hyperparameters.yml", 'r') as file:
+      all_hyperparameters_sets = yaml.safe_load(file)
+      hyperparameters = all_hyperparameters_sets[hyperparameter_set]
+
+    # Define os atributos com base nos hiperparâmetros
+    self.env_id             = hyperparameters.get('env_id')
+    self.epsilon_min        = hyperparameters.get('epsilon_min')
+    self.epsilon_init       = hyperparameters.get('epsilon_init')
+    self.epsilon_decay      = hyperparameters.get('epsilon_decay')
+    self.mini_batch_size    = hyperparameters.get('mini_batch_size')
+    self.replay_memory_size = hyperparameters.get('replay_memory_size')
     
   def run(self, is_training = True, render=False):
     # env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
@@ -16,20 +33,35 @@ class Agent:
 
     num_states = env.observation_space.shape[0]
 
+    rewards_per_episode = []
+
     policy_dqn = DQN(num_states, num_actions).to(device)
 
-    obs, _ = env.reset()
+    # during training we have to know what each action made
+    if is_training:
+      memory = ReplayMemory(self.replay_memory_size)
 
-    while True:
-      # Next action
-      # (feed the observation to your agent here)
-      action = env.action_space.sample()
+    for episode in itertools.count():
+      state, _ = env.reset()
+      terminated = False
+      episode_reward = 0
 
-      # processing
-      obs, reward, terminated, _, info = env.step(action)
+      while not terminated:
+        # Next action
+        # (feed the observation to your agent here)
+        action = env.action_space.sample()
 
-      # checking if the player is still alive
-      if terminated:
-        break
+        # processing
+        new_state, reward, terminated, _, info = env.step(action)
 
-    env.close()
+        # accumulate reward
+        episode_reward += reward
+
+        if is_training:
+          memory.append((state, action, new_state, reward, terminated))
+        
+        # move to new state
+        state = new_state
+
+      rewards_per_episode.append(episode_reward)
+    
